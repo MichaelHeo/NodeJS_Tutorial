b80f576237e449ea69a9bfe24b959f5af88e7fee
Michael Heo <michael.kr95@gmail.com>
2017-10-24 15:57:01 -0200

    alterando mensagem de servidor rodando

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -10,12 +10,14 @@ var connectMYSQL = function() {
     });
   }
 
-  if(process.env.NODE_ENV == 'test'){
+  if(process.env.NODE_ENV == 'production'){
+    //var urlDeConexao = process.env.CLEARDB_DATABASE_URL;
+    //var grupos = urlDeConexao.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/);
     return mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'Michaheo10!',
-      database: 'casadocodigo_nodejs_test'
+      host: 'us-cdbr-iron-east-05.cleardb.net',
+      user: 'b43745cbd4ac7f',
+      password: '93b51f67',
+      database: 'heroku_4ad6e71ed4ab6e2'
     });
   }
 }
